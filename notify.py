82525f0cb76bfa517703a7c82246544e2e0b0d25
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-08-21 18:40:24 -0600

    💬 Notification Options

---
+++ notify.py
@@ -0,0 +1,29 @@
+from secrets import *
+from twilio.rest import Client
+
+sid = 'your'
+token = 'info'
+twilio_number = 'goes'
+my_number = 'here'
+
+client = Client(sid, token)
+
+
+def twilio_call():
+    
+    client.calls.create(url='http://demo.twilio.com/docs/voice.xml',
+                        to=my_number,
+                        from_=twilio_number)
+
+
+def twilio_text(text_message):
+
+    client.messages.create(body=text_message,
+                            from_=twilio_number,
+                            to=my_number)
+
+
+if __name__ == '__main__':
+
+    twilio_call()
+    twilio_text("Time to sell your ADA!")
